d3e94a7c79fe2dde7ad99205fe93f2900cd631f4
UMAIR <um41r0@gmail.com>
2022-05-01 14:04:12 +0500

    user.keymaps

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,51 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+
+-- Normal Mode --
+-- Better window navigation
+keymap("n", "<leader>h", "<C-w>h", opts)
+keymap("n", "<leader>j", "<C-w>j", opts)
+keymap("n", "<leader>k", "<C-w>k", opts)
+keymap("n", "<leader>l", "<C-w>l", opts)
+
+-- Resize with + and -
+keymap("n", "<leader>-", ":vertical resize -2<CR>", opts)
+keymap("n", "<leader>=", ":vertical resize +2<CR>", opts)
+
+-- Navigate between buffers
+keymap("n", "<leader>bb", ":buffers<CR>", opts)
+keymap("n", "<leader>bn", ":bn<CR>", opts)
+keymap("n", "<leader>bp", ":bp<CR>", opts)
+
+-- Insert --
+-- Press jk or kj fast to enter
+keymap("i", "jk", "<ESC>", opts)
+keymap("i", "kj", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts) -- move highlighted text to left
+keymap("v", ">", ">gv", opts) -- move highlighted text to right
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
