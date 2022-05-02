75adb853e8653164394b8b3cf1bffa75a1ae762e
UMAIR <um41r0@gmail.com>
2022-05-02 16:14:16 +0500

    user.lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,12 @@ return packer.startup(function(use)
     -- Shade.nvim: blur splits
     use "sunjon/shade.nvim"
 
+    -- lualine: status bar
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
