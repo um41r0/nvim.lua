fac70204fa0ca18ddeaa037839ffa8632a8483dd
UMAIR <um41r0@gmail.com>
2022-05-29 08:22:31 +0500

    removed flutter-tools

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -39,11 +39,6 @@ keymap("n", "<leader>bp", ":bp<CR>", opts)
 keymap("n", "<leader>t", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<M-t>", "<cmd>Telescope live_grep<cr>", opts)
 
--- flutter
-keymap("n", "<leader>ft", ":Telescope flutter commands <CR>", opts)
-keymap("n", "<leader>fa", ":Lspsaga code_action<CR>", opts)
-keymap("n", "<leader>fe", ":Lspsaga diagnostic_jump_next<CR>", opts)
-
 
 -- Insert --
 -- Press jk or kj fast to enter
@@ -62,27 +57,3 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 
 -- Nvimtree
 keymap("n", "<leader>n", ":NvimTreeToggle<cr>", opts)
-
-
--- onle for flutter but idk how to do this in lsp f me
-
-keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-keymap("n", "<M-.>", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-keymap("x", "<M-.>", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
-keymap("n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-keymap("n", "<leader>ep", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
-keymap(
-    "n",
-    "es",
-    '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
-    opts
-)
-keymap("n", "<leader>en", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
