13a7727b1d8711ea7eb3ce6900a89faf147b8620
UMAIR <um41r0@gmail.com>
2022-05-31 12:48:15 +0500

    winbar: only show filename

--- lua/user/winbar.lua
+++ lua/user/winbar.lua
@@ -4,7 +4,7 @@ vim.api.nvim_set_hl(0, 'WinBarPath', { bg = '#282828', fg = '#a89984' })
 vim.api.nvim_set_hl(0, 'WinBarModified', { bg = '#282828', fg = '#a89984' })
 
 function M.eval()
-    local file_path = vim.api.nvim_eval_statusline('%f', {}).str
+    local file_path = vim.api.nvim_eval_statusline('%t', {}).str
     local modified = vim.api.nvim_eval_statusline('%M', {}).str == '+' and ' + ' or ''
 
     -- file_path = file_path:gsub('/', ' ➤ ')
